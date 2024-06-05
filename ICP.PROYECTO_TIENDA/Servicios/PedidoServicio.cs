05792d4868969dfad6bf13344319e961e3e36f78
Gabriel <andujarpuertagabriel@gmail.com>
2024-06-05 22:54:24 +0200

    feat(tiendaApp-Back): Desarrollo

--- ICP.PROYECTO_TIENDA/Servicios/PedidoServicio.cs
+++ ICP.PROYECTO_TIENDA/Servicios/PedidoServicio.cs
@@ -20,51 +20,73 @@ namespace Icp.TiendaApi.Servicios
             this.mapper = mapper;
         }
 
-        // VER PEDIDOS
+        /// <summary>
+        /// Obtiene todos los pedidos de la base de datos, incluyendo los productos asociados.
+        /// </summary>
+        /// <returns>Una lista de objetos PedidoDTO.</returns>
         public async Task<ActionResult<List<PedidoDTO>>> GetServicio()
         {
-            var pedidosDB = await context.Pedidos
-                .Include(x => x.Productos).ToListAsync();
+            // Obtener todos los pedidos de la base de datos, incluyendo los productos asociados
+            var pedidosDB = await context.Pedidos.Include(x => x.Productos).ToListAsync();
+
+            // Mapear los pedidos a DTO y devolverlos como resultado
             return mapper.Map<List<PedidoDTO>>(pedidosDB);
         }
 
-        // CREAR PEDIDO
+        /// <summary>
+        /// Crea un nuevo pedido en la base de datos.
+        /// </summary>
+        /// <param name="pedidoPostDTO">El objeto PedidoPostDTO que contiene los detalles del pedido.</param>
+        /// <returns>Un ActionResult que indica el resultado de la operación.</returns>
         public async Task<ActionResult> PostServicio([FromForm] PedidoPostDTO pedidoPostDTO)
         {
+            // Establecer el estado del pedido como "Listo para enviar" por defecto
             string estado = "Listo para enviar";
 
-            var existeUsuario = await context.Usuarios
-                .FirstOrDefaultAsync(x => x.IdUsuario == pedidoPostDTO.UsuarioId);
+            // Verificar si el usuario existe en la base de datos
+            var existeUsuario = await context.Usuarios.FirstOrDefaultAsync(x => x.IdUsuario == pedidoPostDTO.UsuarioId);
 
+            // Si el usuario no existe, devolver un error
             if (existeUsuario == null)
             {
+                // Devolver un error indicando que el usuario no existe
                 return BadRequest("No existe el usuario que quiere crear el pedido");
             }
 
-            if (pedidoPostDTO.Articulos.Count == 0 )
+            // Verificar si se enviaron artículos en el pedido
+            if (pedidoPostDTO.Articulos.Count == 0)
             {
-                return BadRequest("No se puede crear un pedido sin articulos");
+                // Devolver un error indicando que no se pueden crear pedidos sin artículos
+                return BadRequest("No se puede crear un pedido sin artículos");
             }
 
+            // Obtener los IDs de los artículos enviados en el pedido
             var articulosIds = pedidoPostDTO.Articulos.Select(x => x.ArticuloId).ToList();
 
+            // Obtener los artículos de la base de datos que corresponden a los IDs enviados
             var articulosDB = await context.Articulos
                  .Where(x => articulosIds.Contains(x.IdArticulo))
                  .ToListAsync();
 
+            // Verificar si alguno de los artículos enviados está eliminado
             foreach (var articulo in articulosDB)
             {
-                if(articulo.EstadoArticulo == "Eliminado")
+                // Si el artículo está eliminado, devolver un error
+                if (articulo.EstadoArticulo == "Eliminado")
                 {
+                    // Devolver un error indicando que uno de los artículos está eliminado
                     return BadRequest("Uno de los artículos enviados está eliminado");
                 }
             }
 
+            // Verificar si se enviaron todos los artículos solicitados
             if (pedidoPostDTO.Articulos.Count != articulosDB.Count)
             {
+                // Devolver un error indicando que no se enviaron todos los artículos solicitados
                 return BadRequest("No existe uno de los artículos enviados");
             }
 
+            // Crear un nuevo objeto Pedido con los detalles del pedido enviado
             var pedidoDB = new Pedido()
             {
                 UsuarioId = pedidoPostDTO.UsuarioId,
@@ -76,56 +98,72 @@ namespace Icp.TiendaApi.Servicios
                 Provincia = pedidoPostDTO.Provincia
             };
 
+            // Agregar los productos del pedido al objeto Pedido
             foreach (var productoDTO in pedidoPostDTO.Articulos)
             {
+                // Crear un nuevo objeto Producto con los detalles del producto enviado
                 var pedidoProducto = new Producto
                 {
                     ArticuloId = productoDTO.ArticuloId,
                     Cantidad = productoDTO.Cantidad
                 };
 
+                // Agregar el producto al pedido
                 pedidoDB.Productos.Add(pedidoProducto);
 
+                // Obtener el artículo y la estantería correspondientes al producto del pedido
                 var estanteriaDB = await context.Almacen
                     .FirstOrDefaultAsync(x => x.ArticuloAlmacen == productoDTO.ArticuloId);
 
                 var articuloDB = await context.Articulos
                     .FirstOrDefaultAsync(x => x.IdArticulo == productoDTO.ArticuloId);
 
+                // Verificar si hay suficiente stock para el producto del pedido
                 if (productoDTO.Cantidad > estanteriaDB.Cantidad)
                 {
+                    // Si no hay suficiente stock, establecer el estado del pedido como "Pendiente de stock"
                     estado = "Pendiente de stock";
                 }
-                else if(productoDTO.Cantidad > estanteriaDB.Cantidad && articuloDB.EstadoArticulo == "Pendiente de eliminar")
+                // Verificar si el artículo está pendiente de eliminar y si hay suficiente stock
+                else if (productoDTO.Cantidad > estanteriaDB.Cantidad && articuloDB.EstadoArticulo == "Pendiente de eliminar")
                 {
+                    // Si no hay suficiente stock, establecer el estado del pedido como "Pendiente de stock"
                     return BadRequest("Uno de los productos enviados no tiene suficiente cantidad y está pendiente de eliminar por lo que no se va a reponer, intente de nuevo con menos cantidad");
                 }
 
+                // Actualizar la cantidad en la estantería
                 estanteriaDB.Cantidad -= pedidoProducto.Cantidad;
             }
 
+            // Establecer el estado del pedido
             pedidoDB.EstadoPedido = estado;
 
+            // Agregar el pedido a la base de datos
             context.Add(pedidoDB);
 
             await context.SaveChangesAsync();
 
+            // Verificar si algún artículo pendiente de eliminar se quedó sin stock
             foreach (var productoDTO in pedidoPostDTO.Articulos)
             {
+                // Obtener la estantería y el artículo correspondientes al producto del pedido
                 var estanteriaDB = await context.Almacen
                     .FirstOrDefaultAsync(x => x.ArticuloAlmacen == productoDTO.ArticuloId);
 
                 var articuloDB = await context.Articulos
                     .FirstOrDefaultAsync(x => x.IdArticulo == productoDTO.ArticuloId);
 
+                // Verificar si el artículo está pendiente de eliminar y si se quedó sin stock
                 if (articuloDB.EstadoArticulo == "Pendiente de eliminar" && estanteriaDB.Cantidad == 0)
                 {
+                    // Establecer el estado del artículo como "Eliminado"
                     articuloDB.EstadoArticulo = "Eliminado";
+
+                    // Actualizar el artículo en la base de datos
+                    await context.SaveChangesAsync();
                 }
             }
-
-            await context.SaveChangesAsync();
-
+            // Devolver un mensaje de éxito
             return Ok();
         }
 
--- ICP.PROYECTO_TIENDA/Servicios/UsuarioServicio.cs
+++ ICP.PROYECTO_TIENDA/Servicios/UsuarioServicio.cs
@@ -12,207 +12,266 @@ using System.Text;
 
 namespace Icp.TiendaApi.Servicios
 {
+    // Servicio de usuario
     public class UsuarioServicio : ControllerBase
     {
+        // Definir el contexto
         private readonly TiendaContext context;
+        // Definir el mapeador
         private IMapper mapper;
+        // Definir la configuración
         private readonly IConfiguration configuration;
 
+        // Constructor de la clase UsuarioServicio
         public UsuarioServicio(TiendaContext context, IMapper mapper, IConfiguration configuration)
         {
+            // Inicializar el contexto
             this.context = context;
+            // Inicializar el mapeador
             this.mapper = mapper;
+            // Inicializar la configuración
             this.configuration = configuration;
-
         }
 
-        // LOGIN
+        /// <summary>
+        /// Realiza el inicio de sesión del usuario.
+        /// </summary>
+        /// <param name="usuarioCredencialesDTO">Las credenciales del usuario.</param>
+        /// <returns>La respuesta de autenticación del usuario.</returns>
         public async Task<ActionResult<UsuarioRespuestaAutenticacionDTO>> LoginServicio(UsuarioCredencialesDTO usuarioCredencialesDTO)
         {
+            // Verificar si el email ya existe
+            var existeEmail = await context.Usuarios.FirstOrDefaultAsync(x => x.Email == usuarioCredencialesDTO.Email);
+
+            // Verificar si el email ya existe
+            if (existeEmail == null)
+            {
+                // Si ya existe, retornar error
+                return BadRequest($"No existe un usuario con el email {usuarioCredencialesDTO.Email}");
+            }
+
+            // Buscar usuario en la base de datos
             var usuarioDB = await context.Usuarios.FirstOrDefaultAsync(x => x.Email == usuarioCredencialesDTO.Email
             && x.Password == usuarioCredencialesDTO.Password);
-
-            var usuarioDTO = mapper.Map<UsuarioDTO>(usuarioDB);
-
+            
+            // Verificar si el usuario existe
             if (usuarioDB == null)
             {
-                return BadRequest("Login incorrecto");
+                // Si no existe, retornar error
+                return BadRequest("Login incorrecto el email no coincide con la contraseña puesta o la contraseña es incorrecta");
             }
-            else if(usuarioDB.EstadoUsuario == "Eliminado"){
-
+            // Verificar si el usuario está eliminado
+            else if (usuarioDB.EstadoUsuario == "Eliminado")
+            {
+                // Si está eliminado, retornar error
                 return BadRequest("El usuario con el que se quiere acceder está eliminado");
             }
             else
             {
+                // Mapear usuario a DTO
+                var usuarioDTO = mapper.Map<UsuarioDTO>(usuarioDB);
+
+                // Si el usuario existe y no está eliminado, construir token
                 return await ConstruirTokenServicio(usuarioDTO);
             }
         }
 
-        // CONSTRUIR TOKEN
+        /// <summary>
+        /// Construye el token de autenticación para el usuario.
+        /// </summary>
+        /// <param name="usuarioDTO">El DTO del usuario.</param>
+        /// <returns>La respuesta de autenticación del usuario.</returns>
         public async Task<ActionResult<UsuarioRespuestaAutenticacionDTO>> ConstruirTokenServicio(UsuarioDTO usuarioDTO)
         {
+            // Definir el rol del usuario
             string rol = "";
-
+            // Si el usuario es administrador
             if (usuarioDTO.Perfil == 1)
             {
                 rol = "Administrador";
             }
+            // Si el usuario es gestor
             else if (usuarioDTO.Perfil == 2)
             {
                 rol = "Gestor";
             }
+            // Si el usuario es operador
             else if (usuarioDTO.Perfil == 3)
             {
                 rol = "Operador";
             }
 
+            // Definir las claims del usuario
             List<Claim> claims = new List<Claim>
             {
                 new Claim(ClaimTypes.Role, rol)
             };
 
+            // Definir la llave de seguridad
             var llave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["llavejwt"]));
 
+            // Definir las credenciales de seguridad
             var creds = new SigningCredentials(llave, SecurityAlgorithms.HmacSha256);
 
+            // Definir la expiración del token
             var expiracion = DateTime.UtcNow.AddYears(1);
 
+            // Definir el token
             var securityToken = new JwtSecurityToken(claims: claims, expires: expiracion, signingCredentials: creds);
 
+            // Definir la respuesta de autenticación
             var respuestaAutenticacionDTO = new UsuarioRespuestaAutenticacionDTO()
             {
+                // Definir el token
                 Token = new JwtSecurityTokenHandler().WriteToken(securityToken),
+
+                // Definir la expiración
                 Expiracion = TimeZoneInfo.ConvertTimeFromUtc(expiracion, TimeZoneInfo.Local)
             };
 
             return respuestaAutenticacionDTO;
         }
 
-        // CHECK TOKEN
-        public ActionResult<bool> CheckToken()
-        {
-            var token = HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
 
-            if (string.IsNullOrEmpty(token))
-            {
-                return Unauthorized();
-            }
-
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.UTF8.GetBytes(configuration["llavejwt"]);
-
-            try
-            {
-                tokenHandler.ValidateToken(token, new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                }, out SecurityToken validatedToken);
-
-                return Ok(true);
-            }
-            catch
-            {
-                return Unauthorized();
-            }
-        }
-
-
-        // VER USUARIOS
+        /// <summary>
+        /// Obtiene todos los usuarios.
+        /// </summary>
+        /// <returns>Una lista de los usuarios</returns>
         public async Task<ActionResult<List<UsuarioDTO>>> GetServicio()
         {
+            // Buscar usuarios en la base de datos
             var usuariosDB = await context.Usuarios.ToListAsync();
 
+            // Mapear usuarios a DTO
             return mapper.Map<List<UsuarioDTO>>(usuariosDB);
         }
 
-        //VER USUARIOS POR ID
+        /// <summary>
+        /// Obtiene un usuario por su Id.
+        /// </summary>
+        /// <param name="IdUsuario">El Id del usuario.</param>
+        /// <returns>El usuario encontrado.</returns>
         public async Task<ActionResult<UsuarioGetPorIdDTO>> GetByIdServicio(int IdUsuario)
         {
-            var usuarioDB = await context.Usuarios
-                .FirstOrDefaultAsync(x => x.IdUsuario == IdUsuario);
+            // Buscar usuario en la base de datos
+            var usuarioDB = await context.Usuarios.FirstOrDefaultAsync(x => x.IdUsuario == IdUsuario);
 
+            // Verificar si el usuario existe
             if (usuarioDB == null)
             {
-                return NotFound();
+                // Si no existe, retornar error
+                return NotFound($"El usuario con el id {IdUsuario} no existe");
             }
 
+            // Mapear usuario a DTO
             return Ok(mapper.Map<UsuarioGetPorIdDTO>(usuarioDB));
         }
 
 
-        // CREAR USUARIOS
         /// <summary>
-        /// ESto es la vaina loca
+        /// Crea un nuevo usuario.
         /// </summary>
-        /// <param name="userCreacionDTO"></param>
-        /// <returns></returns>
-        public async Task<ActionResult> PostServicio([FromForm] UsuarioPostDTO userCreacionDTO)
+        /// <param name="usuarioPostDTO">Los datos del usuario a crear.</param>
+        /// <returns>El resultado de la operación.</returns>
+        public async Task<ActionResult> PostServicio([FromForm] UsuarioPostDTO usuarioPostDTO)
         {
-            var existeNickname = await context.Usuarios.Where(x => x.Nickname == userCreacionDTO.Nickname).ToListAsync();
+            // Verificar si el nickname ya existe
+            var existeNickname = await context.Usuarios.FirstOrDefaultAsync(x => x.Nickname == usuarioPostDTO.Nickname);
 
-            if (existeNickname == null)
+            // Verificar si el nickname ya existe
+            if (existeNickname != null)
             {
-                return BadRequest($"Ya existe un usuario con el nombre {userCreacionDTO.Nickname}");
+                // Si ya existe, retornar error
+                return BadRequest($"Ya existe un usuario con el nombre {usuarioPostDTO.Nickname}");
             }
 
-            var existeEmail = await context.Usuarios.Where(x => x.Email == userCreacionDTO.Email).ToListAsync();
+            // Verificar si el email ya existe
+            var existeEmail = await context.Usuarios.FirstOrDefaultAsync(x => x.Email == usuarioPostDTO.Email);
 
-            if (existeEmail == null)
+            // Verificar si el email ya existe
+            if (existeEmail != null)
             {
-                return BadRequest($"Ya existe un usuario con el email {userCreacionDTO.Email}");
+                // Si ya existe, retornar error
+                return BadRequest($"Ya existe un usuario con el email {usuarioPostDTO.Email}");
             }
 
-            var usuarioDB = mapper.Map<Usuario>(userCreacionDTO);
+            // Mapear DTO a entidad
+            var usuarioDB = mapper.Map<Usuario>(usuarioPostDTO);
 
+            // Definir el estado del usuario
             usuarioDB.EstadoUsuario = "Disponible";
 
+            // Agregar usuario a la base de datos
             context.Add(usuarioDB);
 
+            // Guardar cambios
             await context.SaveChangesAsync();
 
+            // Retornar Ok
             return Ok();
         }
 
-        // MODIFICAR USUARIOS
+        /// <summary>
+        /// Actualiza un usuario existente.
+        /// </summary>
+        /// <param name="usuarioPostDTO">Los datos del usuario a actualizar.</param>
+        /// <param name="IdUsuario">El Id del usuario a actualizar.</param>
+        /// <returns>El resultado de la operación.</returns>
         public async Task<ActionResult> PutServicio([FromForm] UsuarioPostDTO usuarioPostDTO, int IdUsuario)
         {
+            // Buscar el usuario en la base de datos
             var usuarioDB = await context.Usuarios.FirstOrDefaultAsync(x => x.IdUsuario == IdUsuario);
 
+            // Verificar si el usuario existe
             if (usuarioDB == null)
             {
-                return NotFound($"No existe un usuario con el id ${IdUsuario}");
+                // Si no existe, retornar error
+                return NotFound($"El usuario con el id ${IdUsuario} no existe");
             }
 
+            // Mapear los datos del DTO al usuario existente
             usuarioDB = mapper.Map(usuarioPostDTO, usuarioDB);
 
+            // Actualizar el usuario en la base de datos
             context.Update(usuarioDB);
 
+            // Guardar los cambios
             await context.SaveChangesAsync();
 
+            // Retornar Ok
             return Ok();
         }
 
-        // ELIMINAR USUARIOS
+        /// <summary>
+        /// Elimina un usuario existente.
+        /// </summary>
+        /// <param name="IdUsuario">El Id del usuario a eliminar.</param>
+        /// <returns>El resultado de la operación.</returns>
         public async Task<ActionResult> DeleteServicio(int IdUsuario)
         {
+            // Buscar el usuario en la base de datos
             var usuarioDB = await context.Usuarios.FirstOrDefaultAsync(x => x.IdUsuario == IdUsuario);
 
+            // Verificar si el usuario existe
             if (usuarioDB == null)
             {
+                // Si no existe, retornar error
                 return NotFound($"El usuario con el id {IdUsuario} no existe");
             }
-            if(usuarioDB.EstadoUsuario == "Eliminado")
+            // Verificar si el usuario ya está eliminado
+            if (usuarioDB.EstadoUsuario == "Eliminado")
             {
+                // Si ya está eliminado, retornar error
                 return BadRequest($"El usuario con el id {IdUsuario} ya está eliminado");
             }
 
+            // Cambiar el estado del usuario a "Eliminado"
             usuarioDB.EstadoUsuario = "Eliminado";
 
+            // Guardar los cambios en la base de datos
             await context.SaveChangesAsync();
 
+            // Retornar Ok
             return Ok();
         }
     }
